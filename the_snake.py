fdcd43d65cdc959b7909562f7df4d80b66154ebd
Kirill Turochkin <kirill180104@mail.ru>
2024-10-05 13:55:55 +0800

    Added stone and bad_fod + doc_string

--- the_snake.py
+++ the_snake.py
@@ -1,5 +1,5 @@
-from random import choice, randrange
-from typing import Union, Optional
+from random import randrange
+from typing import Optional
 
 import pygame
 
@@ -15,23 +15,32 @@ DOWN = (0, 1)
 LEFT = (-1, 0)
 RIGHT = (1, 0)
 
-# Центр поля
+# Центр поля.
 CENTER = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
 
 # Цвет фона - черный:
 BOARD_BACKGROUND_COLOR = (0, 0, 0)
 
-# Цвет границы ячейки
+# Цвет границы ячейки.
 BORDER_COLOR = (93, 216, 228)
 
-# Цвет яблока
+# Цвет яблока.
 APPLE_COLOR = (255, 0, 0)
 
-# Цвет змейки
+# Цвет змейки.
 SNAKE_COLOR = (0, 255, 0)
+
+# Цвет бомбы.
+BAD_FOOD_COLOR = (255, 0, 255)
+
+# Цвет камня.
+STONE_COLOR = (169, 169, 169)
 
 # Скорость движения змейки:
 SPEED = 20
+
+# Количество камней.
+COUNT_STONE = 3
 
 # Настройка игрового окна:
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32)
@@ -45,16 +54,11 @@ clock = pygame.time.Clock()
 
 # Тут опишите все классы игры.
 class GameObject():
-    """
-    Родительский класс для создания яблок и змеи
+    """Родительский класс для создания яблок и змей.
+
     Атрибуты:
-        1) position: tuple
-             позиция объекта на игровом поле
-        2) body_color: tuple
-             цвет объекта
-    Методы:
-        1) draw -> None
-            метод для переопределения в дочерних классах, для оторисовки
+        position (tuple): Позиция объекта на игровом поле.
+        body_color: (tuple): Цвет объекта.
     """
 
     position: tuple = CENTER
@@ -65,29 +69,20 @@ class GameObject():
         self.body_color = body_color
 
     def draw(self) -> None:
-        # Отрисовка объектов
+        """Метод для отрисовки объектов, предназначен
+        для переопределения в дочерних классах.
+        """
         pass
 
 
-class Apple(GameObject):
-    '''
-    Дочерний класс GameObject
+class Food(GameObject):
+    """Дочерний класс GameObject, предназначен для создания еды для змеи.
+
     Атрибуты:
-        1) body_color: tuple
-            цвет яблока
-        2) position: tuple
-            яблоко появляется в случайном месте на игровом поле
-    Методы:
-        1) __init__ -> None
-            задает цвет яблоку
-            вызвав свойство randomize_position задает случайное расположение
-        2) draw -> None
-            рисует яблоко на поле квадротом 20х20
-    Декоратор:
-        1) randomize_position -> tuple
-            случайно генерирует расположение яблока
-    '''
-    body_color = APPLE_COLOR
+        body_color (tuple): Цвет еды.
+        position (tuple): Расположение еды в случайном месте.
+    """
+
     position = (
         randrange(0, SCREEN_WIDTH, 20),
         randrange(0, SCREEN_HEIGHT, 20),
@@ -96,21 +91,80 @@ class Apple(GameObject):
     def __init__(self, body_color) -> None:
         self.body_color = body_color
         self.position = self.randomize_position
-  
+
     def draw(self) -> None:
+        """Метод для отрисовки еды на игровом поле.
+
+        Аргументы:
+            rect: Объект с расположением и размерами еды.
+        """
         rect = pygame.Rect(self.position, (GRID_SIZE, GRID_SIZE))
         pygame.draw.rect(screen, self.body_color, rect)
         pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
 
     @property
     def randomize_position(self) -> tuple:
+        """Декоратор для генерации случайного расположения еды.
+
+        Возвращает:
+            tuple: Расположение по x и y на игровом поле.
+        """
         return (
             randrange(0, SCREEN_WIDTH, 20),
             randrange(0, SCREEN_HEIGHT, 20),
         )
 
 
+class Stone(Food):
+    """Дочерний класс Food, предназначен для создания препятствия для змеи.
+
+    Атрибуты:
+        body_color (tuple): Цвет камня.
+        list_stone (list): Список камней с их расположением.
+    """
+
+    body_color: tuple = STONE_COLOR
+    count: int = 3
+    list_stones: list = []
+
+    def __init__(self, count):
+        self.count = count
+        self.randomize_position_stone()
+
+    def draw(self) -> None:
+        """Метод для отрисовки камня на игровом поле.
+
+        Аргументы:
+            rect: Объект с расположением и размерами камня.
+        """
+        for stone in self.list_stones:
+            rect = pygame.Rect(stone, (GRID_SIZE, GRID_SIZE))
+            pygame.draw.rect(screen, self.body_color, rect)
+            pygame.draw.rect(screen, BORDER_COLOR, rect, 1)
+
+    def randomize_position_stone(self) -> None:
+        """Метод для генерации случайного расположения камня."""
+        for _ in range(1, self.count + 1):
+            self.list_stones.append(
+                (
+                    randrange(0, SCREEN_WIDTH, 20),
+                    randrange(0, SCREEN_HEIGHT, 20),
+                )
+            )
+
+
 class Snake(GameObject):
+    """Дочерний класс GameObject, предназначен для создания змеи.
+
+    Атрибуты:
+        length (int): Длинна змеи по умолчанию 1.
+        position (list): Тела змеи разделена на сегменты в ввиде кортежа.
+        last (tuple): Старое расположение последнего сегмента змеи.
+        direction (tuple): Текущее направления движение змеи.
+        next_direction (None, tuple): Следующее направление движения змеи.
+        body_color (tuple): Цвет змеи.
+    """
+
     length: int = 1
     positions = [CENTER]
     last: tuple = CENTER
@@ -122,11 +176,15 @@ class Snake(GameObject):
         super().__init__(position, body_color)
 
     def update_direction(self) -> None:
+        """Обновление движения змеи."""
         if self.next_direction:
             self.direction = self.next_direction
             self.next_direction = None
 
     def move(self) -> None:
+        """Метод передвижения змеи по сетке игрового поле,
+        с переходом из одной границы поля в другю.
+        """
         coordinate_x = self.positions[0][0]
         coordinate_y = self.positions[0][1]
         if coordinate_x + self.direction[0] * GRID_SIZE > SCREEN_WIDTH - 1:
@@ -173,6 +231,7 @@ class Snake(GameObject):
         self.positions.pop()
 
     def draw(self) -> None:
+        """Отрисовка змеи."""
         for position in self.positions[:-1]:
             rect = (pygame.Rect(position, (GRID_SIZE, GRID_SIZE)))
             pygame.draw.rect(screen, self.body_color, rect)
@@ -190,9 +249,14 @@ class Snake(GameObject):
 
     @property
     def get_head_position(self) -> tuple:
+        """Декоратор для получения текущего расположения головы змеи."""
         return self.positions[0]
 
     def added_body_snake(self) -> None:
+        """Метод события при котором змеи съедает яблока,
+        в зависимости с какой стороны яблока было съедено
+        к телу добавляется сегмент.
+        """
         if len(self.positions) != self.length:
             if self.length == 1:
                 self.positions.append(
@@ -210,14 +274,19 @@ class Snake(GameObject):
                 )
 
     def reset(self) -> None:
+        """Метод для перезапуска игры,
+        прошлое тело змеи затирается игровым полем
+        и змея начинает движения с центра и 1 сегментом.
+        """
         for cell_body in self.positions:
             last_rect = pygame.Rect(cell_body, (GRID_SIZE, GRID_SIZE))
             pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
         self.positions = [CENTER]
-
+        self.length = 1
 
 
 def handle_keys(game_object) -> None:
+    """Функция для обработки нажатия клавиатуры"""
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -234,16 +303,38 @@ def handle_keys(game_object) -> None:
 
 
 def main():
+    """Функция игры"""
     pygame.init()
     snake = Snake(position=CENTER, body_color=SNAKE_COLOR)
-    apple = Apple(body_color=APPLE_COLOR)
+    apple = Food(body_color=APPLE_COLOR)
+    bad_food = Food(body_color=BAD_FOOD_COLOR)
+    stone = Stone(count=3)
+    stone.draw()
     snake.draw()
     apple.draw()
+    bad_food.draw()
     while True:
         clock.tick(SPEED)
         handle_keys(snake)
         snake.update_direction()
         snake.move()
+        for position_stone in stone.list_stones:
+            if position_stone == snake.get_head_position:
+                snake.reset()
+                stone.draw()
+        if bad_food.position == snake.get_head_position:
+            snake.length -= 1
+            if snake.length == 0:
+                snake.reset()
+                bad_food.position = apple.randomize_position
+            else:
+                bad_food.position = apple.randomize_position
+                last_rect = pygame.Rect(
+                    snake.positions[-1],
+                    (GRID_SIZE, GRID_SIZE),
+                )
+                pygame.draw.rect(screen, BOARD_BACKGROUND_COLOR, last_rect)
+                snake.positions.pop()
         if apple.position == snake.get_head_position:
             snake.length += 1
             apple.position = apple.randomize_position
@@ -253,6 +344,7 @@ def main():
                 snake.reset()
         snake.draw()
         apple.draw()
+        bad_food.draw()
         pygame.display.update()
 
 
